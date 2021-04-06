0ba45d7f1f5cebcc83e91d2e9100a5d1b920f2cd
Shane Alcock <salcock@waikato.ac.nz>
2021-04-05 23:20:13 -0700

    flowtuple-convert: support different interval lengths
     * desired interval length can be specified with -I
     * output file names are now specified as strftime() templates, as
       multiple outputs may result from a single input
     * multiple input files may now be provided, for cases where we
       want to process a large number of files in series (especially
       if the interval size is increasing from FT3).

--- flowtuple-convert/ftconvert.c
+++ flowtuple-convert/ftconvert.c
@@ -130,19 +130,115 @@ typedef struct ft_state {
 struct ftconverter {
     corsaro_avro_writer_t *avrow;
     uint32_t interval;
+    uint32_t conv_interval_size;
+    uint32_t nextinterval;
     Pvoid_t tcp_tuples;
     Pvoid_t udp_tuples;
     Pvoid_t icmp_tuples;
     Pvoid_t other_tuples;
     uint8_t writereq;
+    char *outputtemp;
 };
 
 static void usage(char *prog) {
     fprintf(stderr, "Usage for %s\n\n", prog);
-    fprintf(stderr, "\t%s -V <version> -i <inputfile> -o <outputfile>\n", prog);
+    fprintf(stderr, "\t%s -I <interval size> -V <version> -o <outputfile> <inputfile> [<inputfile> ...]\n", prog);
+    fprintf(stderr, "\nFlowtuples will be written to the output file every <interval size> minutes\n");
     fprintf(stderr, "\n<version> must be set to the flowtuple version of the srouce file\n");
     fprintf(stderr, "\n<inputfile> must be a flowtuple version 2 or 3 file\n");
-    fprintf(stderr, "\n<outputfile> will be an avro file using the flowtuple version 4 format\n");
+    fprintf(stderr, "\n<outputfile> will be the template used to name the avro files that are written using the flowtuple version 4 format\n");
+}
+
+/** Concatenates a string onto an existing string buffer, starting
+ *  from a given pointer. Basically a strcat() where you supply the
+ *  end of the string that you are appending to, rather than having
+ *  the function have to find it beforehand.
+ *
+ *  @param str      The string to add to the existing buffer.
+ *  @param bufp     The location in the buffer to start writing the
+ *                  new string into.
+ *  @param buflim   A pointer to the end of the destination buffer. All
+ *                  concatenation will cease when this pointer is
+ *                  reached, i.e. the resulting string may be truncated.
+ *
+ *  @return the pointer to the character *after* the last character
+ *          written, which can be used for subsequent calls to stradd.
+ */
+static char *stradd(const char *str, char *bufp, char *buflim) {
+    while(bufp < buflim && (*bufp = *str++) != '\0') {
+        ++bufp;
+    }
+    return bufp;
+}
+
+static char *derive_output_name(char *templ, uint32_t interval) {
+	char scratch[9500];
+    char outname[10000];
+    char tsbuf[11];
+    char *ptr, *w, *end;
+    struct timeval tv;
+
+    end = scratch + sizeof(scratch);
+    ptr = templ;
+    w = scratch;
+
+    for (; *ptr; ++ptr) {
+        if (*ptr == '%') {
+            switch (*++ptr) {
+                case '\0':
+                    /* Reached end of naming scheme, stop */
+                    --ptr;
+                    break;
+                case 's':
+                    /* Add unix timestamp */
+                    snprintf(tsbuf, sizeof(tsbuf), "%u", interval);
+                    w = stradd(tsbuf, w, end);
+                    continue;
+                default:
+                    /* Everything should be handled by strftime */
+                    --ptr;
+            }
+        }
+        if (w == end)
+            break;
+        *w++ = *ptr;
+    }
+
+    if (w >= end) {
+        /* Not enough space for the full filename */
+        return NULL;
+    }
+
+    /* Make sure we terminate our string */
+    *w = '\0';
+
+    /* Use strftime() to resolve any remaining format modifiers. Note
+     * that we use UTC for any date-time conversions.
+     */
+    tv.tv_sec = interval;
+    strftime(outname, sizeof(outname), scratch, gmtime(&tv.tv_sec));
+    return strdup(outname);
+
+}
+
+static int start_avro_writer(struct ftconverter *ftdata) {
+
+	char *outname;
+    int ret = 0;
+
+	outname = derive_output_name(ftdata->outputtemp, ftdata->interval);
+    if (outname == NULL) {
+        fprintf(stderr, "Could not derive suitable output file name for interval %u\n", ftdata->interval);
+        return -1;
+    }
+
+    if (corsaro_is_avro_writer_active(ftdata->avrow)) {
+        corsaro_close_avro_writer(ftdata->avrow);
+    }
+
+    ret = corsaro_start_avro_writer(ftdata->avrow, outname, 0);
+    free(outname);
+    return ret;
 }
 
 static inline uint32_t find_quality(struct ftconverter *ftdata, Pvoid_t *map,
@@ -468,17 +564,30 @@ static void convert_ft3(corsaro_avro_reader_t *avreader,
         }
 
         decode_flowtuple_from_avro(record, &ft3);
+        if (ftdata->interval == 0) {
+            ftdata->interval = ft3.interval_ts -
+                    (ft3.interval_ts % (ftdata->conv_interval_size * 60));
+            ftdata->nextinterval = ft3.interval_ts +
+                    (ftdata->conv_interval_size * 60);
+        }
 
-        if (ft3.interval_ts != ftdata->interval) {
-            if (ftdata->interval != 0 && ftdata->writereq != 0) {
-                write_tuples(ftdata, &ftdata->icmp_tuples, 1);
-                write_tuples(ftdata, &ftdata->tcp_tuples, 6);
-                write_tuples(ftdata, &ftdata->udp_tuples, 17);
-                write_tuples(ftdata, &ftdata->other_tuples, 0);
-                ftdata->writereq = 0;
-                fprintf(stderr, "interval %u complete\n", ftdata->interval);
+        while (ft3.interval_ts >= ftdata->nextinterval) {
+            if (ftdata->writereq != 0) {
+                if (start_avro_writer(ftdata) < 0) {
+                    fprintf(stderr, "error starting Avro writer for interval %u\n", ftdata->interval);
+                } else {
+                    write_tuples(ftdata, &ftdata->icmp_tuples, 1);
+                    write_tuples(ftdata, &ftdata->tcp_tuples, 6);
+                    write_tuples(ftdata, &ftdata->udp_tuples, 17);
+                    write_tuples(ftdata, &ftdata->other_tuples, 0);
+                    ftdata->writereq = 0;
+                    fprintf(stderr, "interval %u complete\n",
+                            ftdata->interval);
+                    corsaro_close_avro_writer(ftdata->avrow);
+                }
             }
-            ftdata->interval = ft3.interval_ts;
+            ftdata->interval = ftdata->nextinterval;
+            ftdata->nextinterval += (ftdata->conv_interval_size * 60);
         }
 
         ftdata->writereq = 1;
@@ -538,20 +647,37 @@ static void convert_ft2(flowtuple_record_t *record, void *args) {
     flowtuple_interval_t *ival;
     flowtuple_data_t *data;
     uint8_t proto;
+    uint32_t newinterval;
 
     switch(type) {
         case FLOWTUPLE_RECORD_TYPE_INTERVAL:
-            /* dump all saved flowtuples */
-            if (ftdata->interval != 0 && ftdata->writereq != 0) {
-                write_tuples(ftdata, &ftdata->icmp_tuples, 1);
-                write_tuples(ftdata, &ftdata->tcp_tuples, 6);
-                write_tuples(ftdata, &ftdata->udp_tuples, 17);
-                write_tuples(ftdata, &ftdata->other_tuples, 0);
-                ftdata->writereq = 0;
-                fprintf(stderr, "interval %u complete\n", ftdata->interval);
-            }
             ival = flowtuple_record_get_interval(record);
-            ftdata->interval = ntohl(flowtuple_interval_get_time(ival));
+            newinterval = ntohl(flowtuple_interval_get_time(ival));
+
+            if (ftdata->interval == 0) {
+                ftdata->interval = newinterval - (newinterval % (ftdata->conv_interval_size * 60));
+                ftdata->nextinterval = newinterval + (ftdata->conv_interval_size * 60);
+                break;
+            }
+
+            while (newinterval >= ftdata->nextinterval) {
+                if (ftdata->writereq != 0) {
+                    if (start_avro_writer(ftdata) < 0) {
+                        fprintf(stderr, "error starting Avro writer for interval %u\n", ftdata->interval);
+                    } else {
+                        write_tuples(ftdata, &ftdata->icmp_tuples, 1);
+                        write_tuples(ftdata, &ftdata->tcp_tuples, 6);
+                        write_tuples(ftdata, &ftdata->udp_tuples, 17);
+                        write_tuples(ftdata, &ftdata->other_tuples, 0);
+                        ftdata->writereq = 0;
+                        fprintf(stderr, "interval %u complete\n",
+                                ftdata->interval);
+                        corsaro_close_avro_writer(ftdata->avrow);
+                    }
+                }
+                ftdata->interval = ftdata->nextinterval;
+                ftdata->nextinterval += (ftdata->conv_interval_size * 60);
+            }
             break;
         case FLOWTUPLE_RECORD_TYPE_FLOWTUPLE_DATA:
             data = flowtuple_record_get_data(record);
@@ -579,31 +705,31 @@ int main(int argc, char **argv) {
     flowtuple_errno_t fterr;
     corsaro_avro_reader_t *avreader = NULL;
 
-    char *inputfile = NULL;
     char *outputfile = NULL;
     corsaro_logger_t *logger = NULL;
     struct ftconverter ftdata;
     uint32_t srcversion = 2;
+    uint32_t interval_size = 1;
 
-    int c, ret = 0;
+    int c, ret = 0, i;
     const struct option long_opts[] = {
         { "help", no_argument, NULL, 'h' },
-        { "input", required_argument, NULL, 'i' },
         { "output", required_argument, NULL, 'o' },
+        { "intervalsize", required_argument, NULL, 'I' },
         { "srcversion", required_argument, NULL, 'V' },
         { NULL, 0, NULL, 0 },
     };
 
-    while ((c = getopt_long(argc, argv, ":hi:o:V:", long_opts, NULL)) != -1) {
+    while ((c = getopt_long(argc, argv, ":ho:V:I:", long_opts, NULL)) != -1) {
         switch(c) {
             case 'h':
                 usage(argv[0]);
                 goto endmain;
-            case 'i':
-                inputfile = strdup(optarg);
-                break;
             case 'o':
                 outputfile = strdup(optarg);
+                break;
+            case 'I':
+                interval_size = strtoul(optarg, NULL, 0);
                 break;
             case 'V':
                 srcversion = strtoul(optarg, NULL, 0);
@@ -615,14 +741,8 @@ int main(int argc, char **argv) {
 
     }
 
-    if (inputfile == NULL) {
-        fprintf(stderr, "No input file specified -- halting\n");
-        usage(argv[0]);
-        goto endmain;
-    }
-
     if (outputfile == NULL) {
-        fprintf(stderr, "No output file specified -- halting\n");
+        fprintf(stderr, "No output file template specified -- halting\n");
         usage(argv[0]);
         goto endmain;
     }
@@ -634,6 +754,11 @@ int main(int argc, char **argv) {
         goto endmain;
     }
 
+    if (interval_size == 0) {
+        fprintf(stderr, "Interval size must not be zero!\n");
+        usage(argv[0]);
+        goto endmain;
+    }
 
     logger = init_corsaro_logger("ftconvert", "");
 
@@ -642,28 +767,44 @@ int main(int argc, char **argv) {
         fprintf(stderr, "Unable to create Avro writer\n");
         goto endmain;
     }
+    /*
     if (corsaro_start_avro_writer(ftdata.avrow, outputfile, 0) == -1) {
         fprintf(stderr, "Error starting Avro writer\n");
         goto endmain;
     }
+    */
 
     ftdata.interval = 0;
+    ftdata.nextinterval = 0;
+    ftdata.conv_interval_size = interval_size;
     ftdata.icmp_tuples = NULL;
     ftdata.tcp_tuples = NULL;
     ftdata.udp_tuples = NULL;
     ftdata.other_tuples = NULL;
     ftdata.writereq = 0;
+    ftdata.outputtemp = outputfile;
 
-    if (srcversion == 2) {
-        h = flowtuple_initialize(inputfile, &fterr);
-        flowtuple_loop(h, -1, convert_ft2, &ftdata);
-    } else {
-        avreader = corsaro_create_avro_reader(logger, inputfile);
-        if (avreader == NULL) {
-            fprintf(stderr, "Error starting Avro reader\n");
-            goto endmain;
+    for (i = optind; i < argc; i++) {
+
+        if (srcversion == 2) {
+            h = flowtuple_initialize(argv[i], &fterr);
+            if (!h) {
+                fprintf(stderr, "Error starting libflowtuple reader\n");
+                goto endmain;
+            }
+            flowtuple_loop(h, -1, convert_ft2, &ftdata);
+            flowtuple_release(h);
+            h = NULL;
+        } else {
+            avreader = corsaro_create_avro_reader(logger, argv[i]);
+            if (avreader == NULL) {
+                fprintf(stderr, "Error starting Avro reader\n");
+                goto endmain;
+            }
+            convert_ft3(avreader, &ftdata);
+            corsaro_destroy_avro_reader(avreader);
+            avreader = NULL;
         }
-        convert_ft3(avreader, &ftdata);
     }
 
 endmain:
@@ -675,19 +816,20 @@ endmain:
     }
 
     if (ftdata.writereq) {
-        write_tuples(&ftdata, &(ftdata.icmp_tuples), 1);
-        write_tuples(&ftdata, &(ftdata.tcp_tuples), 6);
-        write_tuples(&ftdata, &(ftdata.udp_tuples), 17);
-        write_tuples(&ftdata, &(ftdata.other_tuples), 0);
+        if (start_avro_writer(&ftdata) >= 0) {
+            write_tuples(&ftdata, &(ftdata.icmp_tuples), 1);
+            write_tuples(&ftdata, &(ftdata.tcp_tuples), 6);
+            write_tuples(&ftdata, &(ftdata.udp_tuples), 17);
+            write_tuples(&ftdata, &(ftdata.other_tuples), 0);
+        } else {
+            fprintf(stderr, "Error starting avro writer for final interval\n");
+        }
     }
 
     if (ftdata.avrow) {
         corsaro_destroy_avro_writer(ftdata.avrow);
     }
 
-    if (inputfile) {
-        free(inputfile);
-    }
     if (outputfile) {
         free(outputfile);
     }
